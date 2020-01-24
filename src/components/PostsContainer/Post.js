c73cc43a128e53ff606ae11c4ceecf190a991544
Brennon Nuckols <brennon@Brennons-MacBook-Pro.local>
2020-01-24 12:01:48 -0700

    Post.js

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,6 +1,6 @@
 // You will add code in this file
 
-import React from "react";
+import React, { useState } from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
@@ -9,26 +9,34 @@ import "./Posts.css";
 
 const Post = props => {
   // set up state for the likes
+  const [likes, setLikes] = useState(props.post2.likes)
+
+  const liked = () => {
+    setLikes(likes + 1);
+  };
 
   return (
     <div className="post-border">
       <PostHeader
-        username={props.post.username}
+        username={props.post2.username}
         thumbnailUrl={
-          props.post.thumbnailUrl
+          props.post2.thumbnailUrl
         }
       />
       <div className="post-image-wrapper">
         <img
           alt="post thumbnail"
           className="post-image"
-          src={props.post.imageUrl}
+          src={props.post2.imageUrl}
         />
       </div>
-      <LikeSection />
+      <LikeSection
+        liked = {liked}
+        likes = {likes}
+      />
       <CommentSection
-        postId={props.post.imageUrl}
-        comments={props.post.comments}
+        postId={props.post2.imageUrl}
+        comments={props.post2.comments}
       />
     </div>
   );
