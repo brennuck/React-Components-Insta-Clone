3185eef89f95d7846b53d51b2438d979c5480f9a
Brennon Nuckols <brennon@Brennons-MacBook-Pro.local>
2020-01-24 12:10:11 -0700

    PostsPage.js

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -4,10 +4,17 @@ import Post from "./Post";
 import "./Posts.css";
 
 // pass the data from App.js down as props then map through the data
-const PostsPage = () => {
+const PostsPage = (props) => {
+  console.log("props", props);
   return (
     <div className="posts-container-wrapper">
       {/* map through data here */}
+      {props.postData.map((pdata) => {
+        <Post
+          post2 = {pdata}
+          key = {pdata.imageUrl}
+        />
+      })}
     </div>
   );
 };
